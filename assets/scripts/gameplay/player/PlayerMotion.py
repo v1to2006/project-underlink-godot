aee276aaaa7cc0f62a371c00352dfb1f3c21f84f
Aleksei Alehin <pixel2006.aa@gmail.com>
2026-02-12 13:05:01 +0200

    Added player movement

---
+++ assets/scripts/gameplay/player/PlayerMotion.py
@@ -0,0 +1,50 @@
+import math
+from py4godot import gdclass
+from py4godot.classes.CharacterBody3D import CharacterBody3D
+from py4godot.classes.Input import Input
+from py4godot.classes.InputEvent import InputEvent
+from py4godot.classes.InputEventMouseMotion import InputEventMouseMotion
+from py4godot.classes.Node3D import Node3D
+
+@gdclass
+class PlayerMotion(CharacterBody3D):
+	speed: float = 10
+	mouse_sensitivity: float = 0.0025
+
+	def _ready(self) -> None:
+		self.input: Input = Input.instance()
+		self.head: Node3D = self.get_node("CameraPivot")
+		self._pitch: float = 0
+		self.input.set_mouse_mode(2)
+	
+	def _unhandled_input(self, event: InputEvent) -> None:
+		if not isinstance(event, InputEventMouseMotion):
+			return
+
+		self.rotate_y(-event.relative.x * self.mouse_sensitivity)
+
+		self._pitch -= event.relative.y * self.mouse_sensitivity
+		self._pitch = max(min(self._pitch, math.radians(90)), math.radians(-90))
+
+		target_rotation = self.head.rotation
+		target_rotation.x = self._pitch
+		self.head.rotation = target_rotation
+	
+	def _physics_process(self, delta: float) -> None:
+		input_vector = self.input.get_vector("move_left", "move_right", "move_back", "move_forward")
+		global_transform = self.global_transform.basis
+
+		direction = (global_transform.x * input_vector.x) + (-global_transform.z * input_vector.y)
+
+		if direction.length() > 0.0:
+			direction = direction.normalized()
+		
+		new_velocity = self.velocity
+		new_velocity.x = direction.x * self.speed
+		new_velocity.z = direction.z * self.speed
+		self.velocity = new_velocity
+
+		self.move_and_slide()
+
+		if self.input.is_action_just_pressed("ui_cancel"):
+			self.input.set_mouse_mode(0)
