8d04018b5654105379bca8fb225a10cadb950aba
RGUFS <juhohaka@metropolia.fi>
2026-03-11 11:02:07 +0200

    Colors & some code changes

--- assets/scenes/MainMenu/Username.py
+++ assets/scenes/MainMenu/Username.py
@@ -6,13 +6,15 @@ from py4godot.classes.Control import Control
 class EnterYourUsername(Control):
 
     def _ready(self) -> None:
-        self.username_input = self.get_node("UsernameInput")
-        self.btn_confirm = self.get_node("Confirm")
-        self.btn_back = self.get_node("Back")
-        self.status_label = self.get_node("StatusLabel")
+        self.username_input = self.get_node("VBoxContainer/UsernameInput")
+        self.btn_confirm = self.get_node("VBoxContainer/Confirm")
+        self.btn_back = self.get_node("VBoxContainer/Back")
+        self.status_label = self.get_node("VBoxContainer/StatusLabel")
 
         self.btn_confirm.pressed.connect(self._on_confirm_pressed)
         self.btn_back.pressed.connect(self._on_back_pressed)
+
+        self.status_label.text = "Waiting for input"
 
     def _on_confirm_pressed(self) -> None:
         username = self.username_input.text.strip()
@@ -21,11 +23,11 @@ class EnterYourUsername(Control):
             self.status_label.text = "Username cannot be empty"
             return
 
-        self.status_label.text = "Creating account..."
+        self.status_label.text = "Username accepted"
+        print("Entered username:", username)
 
-        print("Username entered:", username)
-
-        # Here you will later call Flask
+        # Later:
+        # send username to Flask here
 
     def _on_back_pressed(self) -> None:
         self.get_tree().change_scene_to_file("res://scenes/MainMenu/PlayMenu.tscn")
